e15f0c97725cc47b23049363a9ae2c746640d71d
ralfqbk <2049206692@qq.com>
2018-09-01 10:51:42 +0800

    demo

---
+++ script/public.js
@@ -0,0 +1,15 @@
+// var Domain = 'http://ziye.tunnel.qydev.com/carshop/';
+var Domain = 'https://bbd.hbweiyinqing.cn/carshop/';
+// var Domain = 'http://192.168.1.219/sfapp/f/';
+// var DNS = 'http://ziye.hk1.mofasuidao.cn/sfapp/userfiles/app/';
+// var DNS = 'http://lb.hbweiyinqing.cn/userfiles/app/';
+
+//缓存数据表
+
+/**
+    ' + Domain + '
+    merchant:{
+      "merchantId":7,商家id
+      "merchantName":"车享家汽车养护中心(邯郸盛和路店)"商家名称
+    }
+**/
